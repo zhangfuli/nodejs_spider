1f372f103f991e339984c673db5c88c95b843640
zhangfuli <1171067930@qq.com>
2016-11-10 23:58:56 +0800

    no message

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 var iconv = require('iconv-lite');
+
+//初级
 function capture(){
 	//发送请求抓取的页面
 	request('http://read.qidian.com/BookReader/2OZih9MNQLg1,u0JcxacWQo4ex0RJOkJclQ2.aspx', function (error, response, body) {
@@ -24,4 +26,51 @@ function capture(){
 	  }
 	})
 }
-capture();
+
+//进阶
+function selfRequest(url,callback){
+	var options = {
+		url : url,
+		encoding:null
+	}
+	request(options,callback);
+}
+function request(url){
+	selfRequest(url,function(err,res,body){
+		if(!err&&res.statusCode ==200){
+			//var html = iconv.decode(body,'gb2312'); //转化编码		
+			body = body.toString();
+			var array = body.split("},");
+			fs.writeFile('./somefile.txt',url+'\n',{encoding:"utf8",flag:"a"},function(err){    
+					if(!err){
+						console.log('succeed');
+					}else{
+						throw err;
+					}
+				});
+			for(var j=0;j<array.length;j++){
+				fs.writeFile('./somefile.txt',array[j]+'\n',{encoding:"utf8",flag:"a"},function(err){    
+					if(!err){
+						console.log('succeed');
+					}else{
+						throw err;
+					}
+				});
+			}
+		}
+	})
+}
+for(var i=1;i<=30;i++){
+	var count = 1;
+	count++;
+	if(count == 30){
+
+	}
+	if(i<10){
+		url = "http://yb.upc.edu.cn:8081/test/chengji?studentid=150702030"+i;
+	}
+	else{
+		url = "http://yb.upc.edu.cn:8081/test/chengji?studentid=15070203"+i;
+	}
+	
+}
